01f698b06cc32f24f2f559b924f86e29f69586c4
Anders Busch <andersbusch@gmail.com>
2021-05-14 17:24:24 +0200

    more api docs and typedoc config

--- src/entityManager.ts
+++ src/entityManager.ts
@@ -2,14 +2,37 @@ import { ComponentRegistry } from './componentRegistry';
 import { Entity } from './entity';
 
 export class EntityManager {
+  /**
+   * Entity id cache
+   */
   private nextId: number = 0;
+  
+  /**
+   * Internal pool of entities
+   */
   private entities: Entity[] = [];
+  
+  /**
+   * An instance of ComponentRegistry. Associates the component constructors with component ids, which
+   * are used in entities queries.
+   */
   private componentRegistry: ComponentRegistry = new ComponentRegistry();
 
+  /**
+   * Generates an id number for identifying entities
+   * @returns {number} The next entity id
+   */
   private getNextId(): number {
     return this.nextId++;
   }
 
+  /**
+   * Creates and places an entity in this EntityManager, given the supplied component instances.
+   * @remark The component instances need to have an named constructor associated with them.
+   * @param components A variadic argument list of component instances to be associated with the new
+   * Entity
+   * @returns {Entity} the newly created entity
+   */
   public createEntity(...components: ComponentInstance[]) {
     let entity = new Entity(this.componentRegistry, this.getNextId());
     entity.addComponent(...components);
@@ -17,6 +40,10 @@ export class EntityManager {
     return entity;
   }
 
+  /**
+   * Deletes the supplied entity from the EntityManager array pool.
+   * @param entity Entity to be deleted
+   */
   public deleteEntity(entity: Entity) {
     let index = this.entities.findIndex(
       otherEntity => otherEntity.id === entity.id
@@ -26,6 +53,12 @@ export class EntityManager {
     }
   }
 
+  /**
+   * Get an Entity iterator over a subset of the entities, that has the parsed component types, or
+   * an Entity iterator over all entities, if no component types are supplied.
+   * @param components A set of component constructor to filter for
+   * @returns {Generator<Entity>} An iteratable object instance over the entities
+   */
   public *getEntities(...components: ComponentConstructor[]) {
     if (components.length === 0) {
       for (let e of this.entities) {
@@ -45,10 +78,16 @@ export class EntityManager {
     }
   }
 
+  /**
+   * Get the number of entities in this EntityManager
+   */
   get length() {
     return this.entities.length;
   }
 
+  /**
+   * Iterate over all the entities of this EntityManager
+   */
   *[Symbol.iterator](): IterableIterator<Entity> {
     for (let e of this.entities) {
       yield e;
